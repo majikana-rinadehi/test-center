95c52070ecd5182276f7c95a4183f79ef37b5cd6
Hidenari Nakajima <nh3257991@icloud.com>
2022-05-21 11:49:27 +0900

    functionunitついか

--- src/composable/functions.ts
+++ src/composable/functions.ts
@@ -1,4 +1,5 @@
 import { LocalDate, convert } from '@js-joda/core'
+import { stringifyQuery } from 'vue-router'
 import { FunctionUnit } from './types'
 
 const jsJodaMinusDaysMethod = (input: string): string => {   
@@ -19,7 +20,31 @@ const fugaUnit: FunctionUnit = {
     method: jsJodaPlusDaysMethod
 }
 
+const extractQuery = (input: string) => {
+    const type = input && input.match(/^@(.*?)\?/)
+        ? input.match(/^@(.*?)\?/)![1]
+        : '';
+    const param = input && input.match(/\?(.*?)=/)
+        ? input.match(/\?(.*?)=/)![1]
+        : '';
+    const value = input && input.match(/=(.*?)$/)
+        ? input.match(/=(.*?)$/)![1]
+        : '';
+    const result: {[index : string]: string} = {
+        type,
+        param: param,
+        value
+    }
+    return Object.keys(result).map(v => result[v] as string).join(',')
+}
+
+const extractQueryUnit: FunctionUnit = {
+    comment: 'extract query from "@" start string',
+    method: extractQuery
+}
+
 export const funcUnitList: FunctionUnit[] = [
     hogeUnit,
-    fugaUnit
+    fugaUnit,
+    extractQueryUnit
 ]
